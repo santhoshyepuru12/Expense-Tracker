89b61ba301a1867fe7ec01f31de3d17185d4fa51
santhoshyepuru12 <santhoshyepuru12@gmail.com>
2026-07-20 18:37:45 +0530

    Use Brevo SMTP

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,7 @@ LOGOUT_REDIRECT_URL = "/login/"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
